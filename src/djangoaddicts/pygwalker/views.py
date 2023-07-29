595d023bd6f4a310372c5c495c6160e56ae7d34f
David Slusser <dbslusser@gmail.com>
2023-07-29 16:23:24 -0700

    black, isort, mypy

--- src/djangoaddicts/pygwalker/views.py
+++ src/djangoaddicts/pygwalker/views.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import pygwalker as pyg
-
 from django.shortcuts import render
 from django.views.generic import View
 
 
 class PygView(View):
-    """View to create a PyGWalker visualization interface from a Django queryset. 
+    """View to create a PyGWalker visualization interface from a Django queryset.
     See https://github.com/Kanaries/pygwalker for more information on PyGWalker.
-    
+
     class parameters:
         field_list    - list of model fields to include
         queryset      - queryset providing data available to visualization
@@ -16,7 +15,7 @@ class PygView(View):
         title         - title used on html render
         template_name - template used when rendering page; defaults to: pygwalker/bs5/pyg.html
 
-    example: 
+    example:
 
         from djangoaddicts.pygwalker.views import PygView
         class MyPygView(PygView):
@@ -25,7 +24,8 @@ class PygView(View):
             theme = "light"
             field_list = ["status__name", "customer", "order_id", "created_at", "updated_at", "products"]
     """
-    field_list = []
+
+    field_list: list = []
     queryset = None
     template_name: str = "storemgr/pyg/pyg.html"
     theme: str = "media"
@@ -33,6 +33,5 @@ class PygView(View):
 
     def get(self, request):
         pd_data = pd.DataFrame(list(self.queryset.values(*self.field_list)))
-        context = {"pyg": pyg.walk(pd_data, return_html=True, dark=self.theme),
-                   "title": self.title}
+        context = {"pyg": pyg.walk(pd_data, return_html=True, dark=self.theme), "title": self.title}
         return render(request, self.template_name, context)
