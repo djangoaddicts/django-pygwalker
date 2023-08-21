ee2d820dd8d8ebb5590380a31a30fe00e627e16a
David Slusser <dbslusser@gmail.com>
2023-08-20 21:43:58 -0700

    adding list view with pygwalker link

--- src/djangoaddicts/pygwalker/urls.py
+++ src/djangoaddicts/pygwalker/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from djangoaddicts.pygwalker.views import DynamicCsvPygWalkerView
+from djangoaddicts.pygwalker.views import DynamicCsvPygWalkerView, GenericPygWalkerView, ListToPygWalkerView
 
 app_name = "pygwalker"
 
 urlpatterns = [
     path("", DynamicCsvPygWalkerView.as_view(), name=""),
+    path("csv", DynamicCsvPygWalkerView.as_view(), name="csv"),
+    path("dynamic", DynamicCsvPygWalkerView.as_view(), name="dynamic"),
+    path("file", DynamicCsvPygWalkerView.as_view(), name="file"),
+    path("generic_pyg/<str:app_name>/<str:model_name>/", GenericPygWalkerView.as_view(), name="generic_pyg"), 
+    path("to_pyg/<str:app_name>/<str:model_name>/", ListToPygWalkerView.as_view(), name="to_pyg"), 
 ]
